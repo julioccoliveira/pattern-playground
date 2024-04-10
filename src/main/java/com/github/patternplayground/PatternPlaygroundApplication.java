c411f57b2f54b9e698c798217b08525b9084661a
Júlio Oliveira <juliocc_oliveira@hotmail.com>
2024-04-09 23:23:26 -0300

    First commit

---
+++ src/main/java/com/github/patternplayground/PatternPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.github.patternplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatternPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/patternplayground/PatternPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.patternplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
